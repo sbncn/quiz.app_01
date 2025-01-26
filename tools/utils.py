1b1b41569779e3275395052fe8128c1188af3379
Saban Can <sabancan091011@gmail.com>
2025-01-27 00:31:36 +0100

    utils created

---
+++ tools/utils.py
@@ -0,0 +1,9 @@
+# tools/utils.py
+
+import bcrypt
+
+def hash_password(password):
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+def check_password(password, hashed):
+    return bcrypt.checkpw(password.encode(), hashed.encode())
